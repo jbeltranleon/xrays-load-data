49d8c44b59be65003af0f0e4b7f2ed6f851e563a
Jhonbeltran <jbeltranleon@gmail.com>
2019-06-18 19:49:12 -0500

    - to _

--- xrays_load_data/setup.py
+++ xrays_load_data/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='xrays-load-data',
+setup(name='xrays_load_data',
       version='0.1',
       description='Load the xrays iamges',
       url='http://github.com/jbeltranleon/xrays-load-data',
       author='Jhon Beltran',
       author_email='jbeltranleon@gmail.com',
       license='MIT',
-      packages=['xrays-load-data'],
+      packages=['xrays_load_data'],
       zip_safe=False,
       install_requires=[
        "numpy",
