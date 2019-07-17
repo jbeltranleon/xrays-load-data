80ffebf2fb601bb034a0b926df32cef9d137933b
Jhonbeltran <jbeltranleon@gmail.com>
2019-07-17 18:16:39 -0500

    add dense 121

---
+++ xrays_train_model_dense121/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='xrays_train_model_dense121',
+      version='0.1',
+      description='Train the model using dense121 Application Keras',
+      url='http://github.com/jbeltranleon/xrays-train-model-dense121',
+      author='Jhon Beltran',
+      author_email='jbeltranleon@gmail.com',
+      license='MIT',
+      packages=['xrays_train_model_dense121'],
+      zip_safe=False,
+      install_requires=[)
